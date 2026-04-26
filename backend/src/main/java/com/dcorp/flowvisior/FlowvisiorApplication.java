fa6a5526f47b93a54e2a0623a9931f2e518a43c1
denzlot <wasbaswasbas1@gmail.com>
2026-04-26 22:32:48 +0300

    init project structure

---
+++ backend/src/main/java/com/dcorp/flowvisior/FlowvisiorApplication.java
@@ -0,0 +1,13 @@
+package com.dcorp.flowvisior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowvisiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowvisiorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dcorp/flowvisior/FlowvisiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcorp.flowvisior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowvisiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
